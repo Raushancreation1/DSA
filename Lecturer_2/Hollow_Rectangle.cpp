38715ba7213e7939ee9ec49d4cd12709d87fc5cf
Raushan <raushancreationofficial@.com>
2025-09-15 00:34:35 +0530

    update new codes 15/09/2025

--- Lecturer_2/Hollow_Rectangle.cpp
+++ Lecturer_2/Hollow_Rectangle.cpp
@@ -32,35 +32,55 @@ int main () {
     // }
 
   
-    int rowCount, colCount;
-    cin >> rowCount;
-    cin >> colCount;
+    // int rowCount, colCount;
+    // cin >> rowCount;
+    // cin >> colCount;
 
-    for (int row=0; row<rowCount; row=row+1) {
-        //first row or lasrt row --> print all stars
+    // for (int row=0; row<rowCount; row=row+1) {
+    //     //first row or lasrt row --> print all stars
 
-        if (row==0 || row== rowCount-1) 
+    //     if (row==0 || row== rowCount-1) 
+    //     {
+    //         for (int col=0; col<colCount; col++)
+    //         {
+    //             cout << "* ";
+    //         }
+    //     }
+    //     else{
+    //         //Remainiging middle rows
+    //         //first star
+    //         cout << "* ";
+
+    //         //spaces
+    //         for (int i=0; i<colCount-2; i=i+1)
+    //         {
+    //             cout << "  ";
+    //         }
+    //         //last star
+    //         cout << "* "; 
+    //     }
+        
+    //     cout << endl;
+    // }
+
+
+    int n;
+    cin >> n;
+
+    for (int row=0; row<n; row=row+1)
+    {
+        for (int col=0; col<n; col=col+1)
         {
-            for (int col=0; col<colCount; col++)
+            if (row==0 || row==n-1 || col==0 || col==n-1)
             {
                 cout << "* ";
+            }else{
+                cout<< "  ";
             }
         }
-        else{
-            //Remainiging middle rows
-            //first star
-            cout << "* ";
-
-            //spaces
-            for (int i=0; i<colCount-2; i=i+1)
-            {
-                cout << "  ";
-            }
-            //last star
-            cout << "* "; 
-        }
-        
         cout << endl;
+        
     }
+    
 
 }
--- Lecturer_2/Patterns.cpp
+++ Lecturer_2/Patterns.cpp
@@ -14,29 +14,29 @@ int main () {
     // }
 
 
-    //outer loop for columns
-    for (int col = 0; col<3; col=col+1)
-    {
-        //inner loop for rows
-        for (int row = 0; row<5; row=row+1){
-            cout << "* ";
-        }
-        cout << endl;
-    }
-
-    // int n;
-
-    // cout << "Enter the star size:";
-    // cin >> n;
-    //     //outer loop for rows
-    // for (int row = 0; row<n; row+=1)
+    // //outer loop for columns
+    // for (int col = 0; col<3; col=col+1)
     // {
-    //     //inner loop for columns
-    //     for (int col = 0; col<n; col+=1){
+    //     //inner loop for rows
+    //     for (int row = 0; row<5; row=row+1){
     //         cout << "* ";
     //     }
     //     cout << endl;
     // }
+
+    int n;
+
+    cout << "Enter the star size:";
+    cin >> n;
+        //outer loop for rows
+    for (int row = 0; row<n; row+=1)
+    {
+        //inner loop for columns
+        for (int col = 0; col<n; col+=1){
+            cout << "* ";
+        }
+        cout << endl;
+    }
     
     
 }
