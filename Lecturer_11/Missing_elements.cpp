8822a6727d5d44d47662d5015f27b2c8fac4d6af
raushancreation1 <raushancreationofficial@gmail.com>
2025-12-04 14:50:54 +0530

    Add feature

---
+++ Lecturer_11/Missing_elements.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+using namespace std;
+
+void findMissing(int *a, int n)
+{
+    // visiting array elements and marking visited elements as negative
+    //  for (int i = 0; i < n; i++)
+    //  {
+    //      int index = abs(a[i]);
+    //      if(a[index -1]> 0)
+    //      {
+    //         a[index -1] *= -1;
+    //      }
+    //  }
+    //  for (int i = 0; i < n; i++)
+    //  {
+    //     cout<<a[i]<<" ";
+    //  }
+
+    // all the indexes which are positive means those indexes+1 are missing
+    //  for (int i = 0; i < n; i++)
+    //  {
+    //      if (a[i] > 0)
+    //      {
+    //          cout << i + 1 << " ";
+    //      }
+
+    // }
+
+    // Sorting and swapping method
+    int i = 0;
+    while (i < n)
+    {
+        int index = a[i] - 1;
+        if (a[i] != a[index])
+        {
+            swap(a[i], a[index]);
+        }
+        else
+        {
+            i++;
+        }
+    }
+    // for (int i = 0; i < n; i++)
+    // {
+    //     cout<<a[i]<<" ";
+    // }
+    // cout<<endl;
+
+    for (int i = 0; i < n; i++)
+    {
+        if (a[i]!=i+1)
+        {
+            cout<<i+1<<" ";
+        }
+        
+    }
+    cout<<endl;
+    
+}
+
+
+int main()
+{
+    int n;
+    // int a[] = {1,3,5,3,4};
+    int a[] = {5, 3, 3, 3, 1};
+    n = sizeof(a) / sizeof(int);
+    findMissing(a, n);
+    return 0;
+}
