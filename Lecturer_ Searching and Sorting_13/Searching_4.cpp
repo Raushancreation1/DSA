ae61f89cac56c7d3b96c5ba52d994acb6bcf673f
raushancreation1 <raushancreationofficial@gmail.com>
2026-01-10 23:43:43 +0530

    Added DSA C++ solutions

---
+++ Lecturer_ Searching and Sorting_13/Searching_4.cpp
@@ -0,0 +1,49 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int lastOcc(vector<int> arr, int target){
+    int s=0;
+    int e= arr.size() -1;
+
+    int mid = s+ (e - s)/2;
+    int ans = -1;
+
+    while (s <= e)
+    {
+       if (arr[mid] == target)
+       {
+        // ans store
+        ans = mid;
+
+        // search in right part
+        s = mid +1;
+       }
+        else if (target < arr[mid])
+        {
+           //left part
+           e = mid -1;
+        }
+        else if (target > arr[mid])
+        {
+            // right part
+            s = mid +1;
+        }
+        mid = s + (e - s)/2;
+    }
+    return ans;
+    
+}
+
+int main() {
+
+    vector<int> v{1,3,7,7,7,7,7,7,7,7,7,7,7,7,9};
+
+    int target = 7;
+
+
+    int ans = lastOcc(v, target);
+    cout << "Last Occurence of "<< ans << endl;
+
+    return 0;
+}
